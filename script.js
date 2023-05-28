8de16ae4448d89c361758c0845136f13c69aa419
mohamed haddou <haddou0mohamed@gmail.com>
2023-05-28 22:08:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/** @format */
+// Line Chart :
+
+new Chart(document.getElementById("line-chart"), {
+  type: "line",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
+    datasets: [
+      {
+        label:'Total Earning ',
+         backgroundColor: "#6023D6",
+        borderColor: "#6023D6",
+        data: [12, 19, 3, 5, 12, 3, 55, 13],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+        legend: {
+          position: "bottom",
+        },
+      },
+  },
+});
